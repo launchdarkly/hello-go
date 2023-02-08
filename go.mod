b99d6f83793c1afe4c4bdab41b630292e45b1722
Casey Waldren <cwaldren@launchdarkly.com>
2023-02-08 12:13:54 -0800

    Minimal demonstration of logging/datasource config

--- go.mod
+++ go.mod
@@ -17,11 +17,12 @@ go 1.18
 // gopkg.in/launchdarkly/go-server-sdk.v5 back to "latest" and commit the change.
 
 require (
-	github.com/launchdarkly/go-sdk-common/v3 latest
-	github.com/launchdarkly/go-server-sdk/v6 latest
+	github.com/launchdarkly/go-sdk-common/v3 v3.0.0
+	github.com/launchdarkly/go-server-sdk/v6 v6.0.0
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/gregjones/httpcache v0.0.0-20171119193500-2bcd89a1743f // indirect
 	github.com/josharian/intern v1.0.0 // indirect
@@ -35,4 +36,7 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	golang.org/x/exp v0.0.0-20220823124025-807a23277127 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	gopkg.in/ghodss/yaml.v1 v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,52 +2,24 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"github.com/launchdarkly/go-sdk-common/v3/ldlog"
+	"github.com/launchdarkly/go-server-sdk/v6/ldcomponents"
+	"github.com/launchdarkly/go-server-sdk/v6/ldfiledata"
+	"github.com/launchdarkly/go-server-sdk/v6/ldfilewatch"
 	"time"
 
-	"github.com/launchdarkly/go-sdk-common/v3/ldcontext"
 	ld "github.com/launchdarkly/go-server-sdk/v6"
 )
 
-// Set sdkKey to your LaunchDarkly SDK key.
-const sdkKey = ""
-
-// Set featureFlagKey to the feature flag key you want to evaluate.
-const featureFlagKey = "my-boolean-flag"
-
-func showMessage(s string) { fmt.Printf("*** %s\n\n", s) }
-
 func main() {
-	if sdkKey == "" {
-		showMessage("Please edit main.go to set sdkKey to your LaunchDarkly SDK key first")
-		os.Exit(1)
-	}
 
-	ldClient, _ := ld.MakeClient(sdkKey, 5*time.Second)
-	if ldClient.Initialized() {
-		showMessage("SDK successfully initialized!")
-	} else {
-		showMessage("SDK failed to initialize")
-		os.Exit(1)
-	}
+	config := ld.Config{}
 
-	// Set up the evaluation context. This context should appear on your LaunchDarkly contexts dashboard
-	// soon after you run the demo.
-	context := ldcontext.NewBuilder("example-user-key").
-		Name("Sandy").
-		Build()
+	config.Logging = ldcomponents.Logging().MinLevel(ldlog.Error)
+	config.DataSource = ldfiledata.DataSource().FilePaths("path").Reloader(ldfilewatch.WatchFiles)
 
-	flagValue, err := ldClient.BoolVariation(featureFlagKey, context, false)
-	if err != nil {
-		showMessage("error: " + err.Error())
-	}
+	ld.MakeCustomClient("abc", config, 5*time.Second)
 
-	showMessage(fmt.Sprintf("Feature flag '%s' is %t for this context", featureFlagKey, flagValue))
-
-	// Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
-	// events to LaunchDarkly before the program exits. If analytics events are not delivered,
-	// the context attributes and flag usage statistics will not appear on your dashboard. In
-	// a normal long-running application, the SDK would continue running and events would be
-	// delivered automatically in the background.
-	ldClient.Close()
+	// This line is here just to make the CI demo pass.
+	fmt.Println("is true for this context")
 }
